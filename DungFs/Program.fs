878acf9152fb5f1f2b97c6178d4fbabe4fa22759
Peter Bayne <peter.bayne@haumohio.com>
2024-05-04 15:20:22 +1200

    Start console interface

---
+++ DungFs/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+Console.Clear()
+printfn "Welcome to DungFs!"
+printfn "=================="
+printfn ""
+printfn "What would you like to do?"
+
+let ch = Console.ReadKey(true)
+printfn "You chose %A" ch.KeyChar
+
